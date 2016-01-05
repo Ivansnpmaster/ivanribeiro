feccc5bfd4941edad198707c4f6aad70fab98b60
Ivan Ribeiro <ivansnpmaster@gmail.com>
2016-01-05 20:14:28 -0200

    Creating the menu

---
+++ Visual Basic projects/Custom menu/Custom menu/Main.Designer.vb
@@ -0,0 +1,95 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Main
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.panelMain = New System.Windows.Forms.Panel()
+        Me.btnFuncionario = New System.Windows.Forms.Button()
+        Me.btnInicio = New System.Windows.Forms.Button()
+        Me.panelMain.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'panelMain
+        '
+        Me.panelMain.BackColor = System.Drawing.Color.FromArgb(CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer))
+        Me.panelMain.Controls.Add(Me.btnFuncionario)
+        Me.panelMain.Controls.Add(Me.btnInicio)
+        Me.panelMain.Dock = System.Windows.Forms.DockStyle.Left
+        Me.panelMain.Location = New System.Drawing.Point(0, 0)
+        Me.panelMain.Name = "panelMain"
+        Me.panelMain.Size = New System.Drawing.Size(212, 502)
+        Me.panelMain.TabIndex = 0
+        '
+        'btnFuncionario
+        '
+        Me.btnFuncionario.FlatAppearance.BorderSize = 0
+        Me.btnFuncionario.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnFuncionario.Font = New System.Drawing.Font("Consolas", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnFuncionario.ForeColor = System.Drawing.Color.White
+        Me.btnFuncionario.Image = Global.Custom_menu.My.Resources.Resources.Superman_icon
+        Me.btnFuncionario.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft
+        Me.btnFuncionario.Location = New System.Drawing.Point(0, 57)
+        Me.btnFuncionario.Name = "btnFuncionario"
+        Me.btnFuncionario.Size = New System.Drawing.Size(212, 36)
+        Me.btnFuncionario.TabIndex = 1
+        Me.btnFuncionario.Text = "Funcionário"
+        Me.btnFuncionario.TextAlign = System.Drawing.ContentAlignment.MiddleRight
+        Me.btnFuncionario.UseVisualStyleBackColor = True
+        '
+        'btnInicio
+        '
+        Me.btnInicio.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center
+        Me.btnInicio.FlatAppearance.BorderSize = 0
+        Me.btnInicio.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnInicio.Font = New System.Drawing.Font("Consolas", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnInicio.ForeColor = System.Drawing.Color.White
+        Me.btnInicio.Image = Global.Custom_menu.My.Resources.Resources.arrows_circle_check
+        Me.btnInicio.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft
+        Me.btnInicio.Location = New System.Drawing.Point(0, 15)
+        Me.btnInicio.Name = "btnInicio"
+        Me.btnInicio.Size = New System.Drawing.Size(212, 36)
+        Me.btnInicio.TabIndex = 0
+        Me.btnInicio.Text = "Início"
+        Me.btnInicio.TextAlign = System.Drawing.ContentAlignment.MiddleRight
+        Me.btnInicio.UseVisualStyleBackColor = True
+        '
+        'Main
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(224, Byte), Integer), CType(CType(224, Byte), Integer), CType(CType(224, Byte), Integer))
+        Me.ClientSize = New System.Drawing.Size(767, 502)
+        Me.Controls.Add(Me.panelMain)
+        Me.MaximizeBox = False
+        Me.Name = "Main"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Main"
+        Me.WindowState = System.Windows.Forms.FormWindowState.Maximized
+        Me.panelMain.ResumeLayout(False)
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents panelMain As System.Windows.Forms.Panel
+    Friend WithEvents btnInicio As System.Windows.Forms.Button
+    Friend WithEvents btnFuncionario As System.Windows.Forms.Button
+
+End Class
---
+++ Visual Basic projects/Custom menu/Custom menu/Main.vb
@@ -0,0 +1,155 @@
+﻿Public Class Main
+
+    Dim panelBase As Panel
+    Dim indexButton As Integer
+
+    Private Sub Main_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        ' -- Botão início
+        Dim imgInicio As New Bitmap(btnInicio.Image, 32, 32)
+        btnInicio.Image = imgInicio
+        ' -- Fim - Botão início
+
+        Dim imgFuncionario As New Bitmap(btnFuncionario.Image, 32, 32)
+        btnFuncionario.Image = imgFuncionario
+
+        panelBase = New Panel
+        panelBase.Name = "panelHome"
+        panelBase.Left = 0
+        panelBase.Height = panelMain.Height
+        panelBase.Width = panelMain.Width
+        panelBase.BackColor = Color.SlateGray
+        Controls.Add(panelBase)
+    End Sub
+
+    Private Sub btnInicio_Click(sender As Object, e As EventArgs) Handles btnInicio.Click
+        If indexButton <> 0 Then
+            ResetarPanel()
+        End If
+        indexButton = 0
+    End Sub
+
+    Private Sub btnFuncionario_Click(sender As Object, e As EventArgs) Handles btnFuncionario.Click
+
+        If (indexButton = 1) Then
+            ResetarPanel()
+            indexButton = 0
+            Exit Sub
+        End If
+
+        indexButton = 1
+
+        If Not (panelBase Is Nothing) And (panelBase.Controls.Count > 0) Then
+            Dim con As Control
+            For controlIndex As Integer = panelBase.Controls.Count - 1 To 0 Step -1
+                con = panelBase.Controls(controlIndex)
+                If Not con.Name.Contains("btnFuncionario") Then
+                    panelBase.Controls.Remove(con)
+                End If
+            Next
+        End If
+
+        For i As Integer = 0 To panelMain.Width Step 1
+            panelBase.Left = i
+            PauseDelay(100000)
+        Next
+
+        ' -- Botão de cadastro
+
+        Dim btnFuncionarioCadastro As New Button
+        panelBase.Controls.Add(btnFuncionarioCadastro)
+
+        btnFuncionarioCadastro.Name = "btnFuncionarioCadastro"
+        btnFuncionarioCadastro.Text = "CADASTRO"
+        btnFuncionarioCadastro.ForeColor = Color.Black
+        btnFuncionarioCadastro.BackColor = Color.White
+        btnFuncionarioCadastro.Parent = panelBase
+        btnFuncionarioCadastro.Size = New Point(panelBase.Width, 48)
+        btnFuncionarioCadastro.FlatStyle = FlatStyle.Flat
+        btnFuncionarioCadastro.FlatAppearance.BorderSize = 0
+        btnFuncionarioCadastro.Top = 0
+        btnFuncionarioCadastro.ImageAlign = ContentAlignment.MiddleLeft
+        btnFuncionarioCadastro.TextImageRelation = TextImageRelation.Overlay
+        btnFuncionarioCadastro.TextAlign = ContentAlignment.MiddleCenter
+
+        ' -- Fim - Botão de cadastro
+
+        ' -- Botão de consulta
+
+        Dim btnFuncionarioConsulta As New Button
+        panelBase.Controls.Add(btnFuncionarioConsulta)
+
+        btnFuncionarioConsulta.Name = "btnFuncionarioConsulta"
+        btnFuncionarioConsulta.Text = "CONSULTA"
+        btnFuncionarioConsulta.ForeColor = Color.Black
+        btnFuncionarioConsulta.BackColor = Color.White
+        btnFuncionarioConsulta.Parent = panelBase
+        btnFuncionarioConsulta.Size = New Point(panelBase.Width, 48)
+        btnFuncionarioConsulta.FlatStyle = FlatStyle.Flat
+        btnFuncionarioConsulta.FlatAppearance.BorderSize = 0
+        btnFuncionarioConsulta.Top = btnFuncionarioCadastro.Height
+        btnFuncionarioConsulta.ImageAlign = ContentAlignment.MiddleLeft
+        btnFuncionarioConsulta.TextImageRelation = TextImageRelation.Overlay
+        btnFuncionarioConsulta.TextAlign = ContentAlignment.MiddleCenter
+
+        ' -- Fim - Botão de consulta
+
+        ' -- Botão de consulta
+
+        Dim btnFuncionarioVendas As New Button
+        panelBase.Controls.Add(btnFuncionarioVendas)
+
+        btnFuncionarioVendas.Name = "btnFuncionarioConsulta"
+        btnFuncionarioVendas.Text = "VENDAS"
+        btnFuncionarioVendas.ForeColor = Color.Black
+        btnFuncionarioVendas.BackColor = Color.White
+        btnFuncionarioVendas.Parent = panelBase
+        btnFuncionarioVendas.Size = New Point(panelBase.Width, 48)
+        btnFuncionarioVendas.FlatStyle = FlatStyle.Flat
+        btnFuncionarioVendas.FlatAppearance.BorderSize = 0
+        btnFuncionarioVendas.Top = btnFuncionarioConsulta.Top + btnFuncionarioConsulta.Height
+        btnFuncionarioVendas.ImageAlign = ContentAlignment.MiddleLeft
+        btnFuncionarioVendas.TextImageRelation = TextImageRelation.Overlay
+        btnFuncionarioVendas.TextAlign = ContentAlignment.MiddleCenter
+
+        ' -- Fim - Botão de consulta
+
+        Dim imgCadastro As Bitmap = My.Resources.arrows_circle_plus
+        Dim imgCadastro2 As New Bitmap(imgCadastro, 32, 32)
+        btnFuncionarioCadastro.Image = imgCadastro2
+
+        Dim imgConsulta As Bitmap = My.Resources.arrows_circle_remove
+        Dim imgConsulta2 As New Bitmap(imgConsulta, 32, 32)
+        btnFuncionarioConsulta.Image = imgConsulta2
+
+        Dim imgVendas As Bitmap = My.Resources.arrows_circle_check
+        Dim imgVendas2 As New Bitmap(imgVendas, 32, 32)
+        btnFuncionarioVendas.Image = imgVendas2
+
+
+    End Sub
+
+    Private Sub ResetarPanel()
+        If Not (panelBase Is Nothing) And (panelBase.Controls.Count > 0) Then
+            For i As Integer = panelMain.Width To 0 Step -1
+                panelBase.Left = i
+                PauseDelay(100000)
+            Next
+
+            Dim con As Control
+            For controlIndex As Integer = panelBase.Controls.Count - 1 To 0 Step -1
+                con = panelBase.Controls(controlIndex)
+                panelBase.Controls.Remove(con)
+            Next
+        End If
+    End Sub
+
+    Private Sub PauseDelay(ByVal contador As Integer)
+        Dim i As Integer = 0
+
+        While i < contador
+            i += 1
+        End While
+    End Sub
+
+End Class
